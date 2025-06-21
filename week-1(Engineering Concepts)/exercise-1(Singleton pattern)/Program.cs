0e5f5e5bf2dd5d9c0bd1fc38b72d1317b85204fb
Barath s <112713529+freqcoder19@users.noreply.github.com>
2025-06-21 10:38:44 +0530

    Add files via upload

--- week-1(Engineering Concepts)/exercise-1(Singleton pattern)/Program.cs
+++ week-1(Engineering Concepts)/exercise-1(Singleton pattern)/Program.cs
@@ -1,1 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
 
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            logger1.Log("This is the first message.");
+
+            Logger logger2 = Logger.GetInstance();
+            logger2.Log("This is the second message.");
+
+            // Check if both references are the same
+            Console.WriteLine("Are both logger instances the same? " + (logger1 == logger2));
+        }
+    }
+}
