f43d5ea7795cb935c1ce29baeb33338115067f0d
Barath s <112713529+freqcoder19@users.noreply.github.com>
2025-06-21 09:00:19 +0530

    change 8

---
+++ exercise-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactoryMethodPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentFactory factory;
+
+            factory = new WordDocumentFactory();
+            Document wordDoc = factory.CreateDocument();
+            wordDoc.Open();
+
+            factory = new PdfDocumentFactory();
+            Document pdfDoc = factory.CreateDocument();
+            pdfDoc.Open();
+
+            factory = new ExcelDocumentFactory();
+            Document excelDoc = factory.CreateDocument();
+            excelDoc.Open();
+        }
+    }
+}
